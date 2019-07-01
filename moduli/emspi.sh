bdd5f3774d20393b8e49d54b71f3dd0989c50a19
Cignoraptor-ita <root4sec6per3@openmailbox.org>
2019-07-01 14:57:09 +0200

    Update emspi.sh

--- moduli/emspi.sh
+++ moduli/emspi.sh
@@ -35,10 +35,30 @@ echo " "
 echo -e "\e[00;34mSearching Gmail.com | Yahoo.com | Libero.it emails associated...\e[00m"
 sleep 0.5
 
+
+
+#---
+links2 -http.fake-user-agent "Mozilla/5.0 (X11; FreeBSD amd64; rv:26.0) Gecko/20100101 Firefox/26.0" -ssl.certificates 0 -dump startpage.com/do/dsearch?query=$target+"@gmail.com" > gm.txt
+sleep 0.3
+links2 -http.fake-user-agent "Mozilla/5.0 (X11; FreeBSD amd64; rv:27.0) Gecko/20100101 Firefox/27.0" -ssl.certificates 0 -dump startpage.com/do/dsearch?query=host%3A$target+"@yahoo.com" > yh.txt
+sleep 0.5
+links2 -http.fake-user-agent "Mozilla/5.0 (X11; FreeBSD amd64; rv:30.0) Gecko/20100101 Firefox/30.0" -ssl.certificates 0 -dump startpage.com/do/dsearch?query=$target+"@libero.it" > li.txt
+#---
+
+links2 -http.fake-user-agent "Mozilla/5.0 (X11; FreeBSD amd64; rv:26.0) Gecko/20100101 Firefox/26.0" -ssl.certificates 0 -dump google.it/search?q=$target+"@gmail.com" >> gm.txt
+sleep 1
+
+links2 -http.fake-user-agent "Mozilla/5.0 (X11; FreeBSD amd64; rv:26.0) Gecko/20100101 Firefox/27.0" -ssl.certificates 0 -dump google.de/search?q=$target+"@yahoo.com" >> yh.txt
+
+sleep 1
+
+links2 -http.fake-user-agent "Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36." -ssl.certificates 0 -dump google.nl/search?q=$target+"@libero.it" >> li.txt
+
+
 echo "======"
 echo "GMAIL"
-echo "\e[00;34m------\e[00m"
-links2 -http.fake-user-agent "Mozilla/5.0 (X11; FreeBSD amd64; rv:26.0) Gecko/20100101 Firefox/26.0" -ssl.certificates 0 -dump startpage.com/do/dsearch?query=host%3A$target+"@gmail.com" | grep --color -Eio "[a-zA-Z0-9.]+@[a-z0-9]+.[a-z]+"
+echo -e "\e[00;34m------\e[00m"
+cat gm.txt | grep --color -Eio "[a-zA-Z0-9.]+@[a-z0-9]+.[a-z]+"
 echo -e "\e[01;34m------\e[00m"
 echo "======"
 
@@ -47,8 +67,9 @@ sleep 0.4
 echo "====="
 echo "YAHOO"
 echo -e "\e[01;34m------\e[00m"
-sleep 0.7
-links2 -http.fake-user-agent "Mozilla/5.0 (X11; FreeBSD amd64; rv:27.0) Gecko/20100101 Firefox/27.0" -ssl.certificates 0 -dump startpage.com/do/dsearch?query=host%3A$target+"@yahoo.com" | grep --color -Eio "[a-zA-Z0-9.]+@[a-z0-9]+.[a-z]+"
+cat yh.txt | grep --color -Eio "[a-zA-Z0-9.]+@[a-z0-9]+.[a-z]+"
+sleep 0.2
+
 echo -e "\e[01;34m------\e[00m"
 echo "====="
 
@@ -56,9 +77,9 @@ echo " "
 echo "========="
 echo "LIBERO.IT"
 echo -e "\e[00;34m---------\e[00m"
-sleep 0.6
-links2 -http.fake-user-agent "Mozilla/5.0 (X11; FreeBSD amd64; rv:30.0) Gecko/20100101 Firefox/30.0" -ssl.certificates 0 -dump startpage.com/do/dsearch?query=host%3A$target+"@libero.it" | grep --color -Eio "[a-zA-Z0-9.]+@[a-z0-9]+.[a-z]+"
+cat li.txt | grep --color -Eio "[a-zA-Z0-9.]+@[a-z0-9]+.[a-z]+"
 echo -e "\e[00;34m---------\e[00m"
 echo "========="
 echo " "
-rm fs.html && rm smt.txt
+rm fs.html && rm smt.txt && rm li.txt
+rm yh.txt && rm gm.txt
